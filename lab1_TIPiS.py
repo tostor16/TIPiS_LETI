4b411ac5ab01f7721f95e48accd72c04f1a13f9b
tostor16 <145290448+tostor16@users.noreply.github.com>
2025-10-03 00:43:49 +0300

    Update lab1_TIPiS.py
    Добавил отображение гистограммы
    Написал больше комментариев для понимания

--- lab1_TIPiS.py
+++ lab1_TIPiS.py
@@ -1,27 +1,34 @@
-from ucimlrepo import fetch_ucirepo 
+from ucimlrepo import fetch_ucirepo
 import pandas as pd
+import matplotlib.pyplot as plt
 
-def greater_income():
-    full_df = pd.concat([X_df, Y_df])
-    count_income = full_df[full_df['income'] == '>50K']['sex'].value_counts()
-    return "мужчин" if count_income.get('Male', 0) > count_income.get('Female', 0) else "женщин"
+# Получаем DataSet
+adult = fetch_ucirepo(id=2) 
+  
+# Извлекаем DataFrame
+X = adult.data.features # DataFrame с исхожными данными
+y = adult.data.targets  # DataFrame с полученными данными
 
-def replace_with_mode():
-    for column in X_df.columns:
-        if X_df[column].isnull().any():
-            mode_values = X_df[column].mode()
-            if not mode_values.empty:
-                mode_value = mode_values.iloc[0]
-                X_df[column] = X_df[column].fillna(mode_value)
-                print(f"Заполнено {column}: мода = {mode_value}")
-    
-adult = fetch_ucirepo(id=2)  
-X_df = pd.DataFrame(adult.data.features) 
-Y_df = pd.DataFrame(adult.data.targets) 
-print(f"1. Число столбцов {X_df.shape[1]}", end='\n\n\n') 
-print("2. Пропуски в данных:", X_df.isnull().sum(), sep='\n', end='\n\n\n')
-print(f"3. Количество уникальных значений в race {X_df['race'].nunique()}", end='\n\n\n')
-print(f"4. Медиана hours-per-week {X_df['hours-per-week'].mean()}", end='\n\n\n')
-print(f"5. Зарплата больше у {greater_income()}", end='\n\n\n')
-print("6. Замена пустых значений на моду в каждом столбце")
-replace_with_mode()
+df = pd.concat([X, y], axis=1)
+
+
+print(f"кол-во столбцов (объединенной DataFrame):\t {df.shape[1]}\n") # кол-во столбцов (объединенной DataFrame)
+print(f"столбцы где пропущеены зн-ия:\n {df.isnull().sum()}\n") # столбцы где пропущеены зн-ия
+print(f"кол-во уникальных значений в столбце race:\t {df.race.unique()}\n") # кол-во уникальных значений в столбце race
+print(f"медиана hours-per-week:\t{df['hours-per-week'].median()}\n") # медиана hours-per-week
+df["hours-per-week"].hist()
+plt.show()
+
+df_select = df[df['income'] == '>50K']
+man = len(df_select[df_select['sex'] == "Male"])
+female = len(df_select[df_select['sex'] == "Female"])
+difference = man - female
+print(f"Мужчин зарабатывающик >50K больше на:\t{difference}\n")
+
+mode_workclass = df["workclass"].mode()[0] # находим самое распространенное зн-ие
+df["workclass"] = df["workclass"].fillna(mode_workclass) # заполняем пропуски
+mode_native_country = df["native-country"].mode()[0] # находим самое распространенное зн-ие
+df["native-country"] = df["native-country"].fillna(mode_native_country) # заполняем пропуски
+mode_occupation = df["occupation"].mode()[0] # находим самое распространенное зн-ие
+df["occupation"] = df["occupation"].fillna(mode_occupation) # заполняем пропуски
+print(f"Пропущенные зн-ия после заполнения mode: \n{df.isnull().sum()}", sep = "\n") # столбцы где пропущеены зн-ия
